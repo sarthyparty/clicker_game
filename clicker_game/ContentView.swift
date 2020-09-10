a52ca62b911083715b7a8332311cf5636b95d743
Sarthak Agrawal <sarthavader@gmail.com>
2020-09-10 10:40:23 -0500

    Initial Commit

---
+++ clicker_game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  clicker_game
+//
+//  Created by 64000774 on 9/10/20.
+//  Copyright © 2020 64000774. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
